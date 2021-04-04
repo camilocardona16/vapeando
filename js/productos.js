f59553762a1a70a02fc3d9c83b19468274e6294f
Andres Camilo Cardona Londoño <camilocardona2224@gmail.com>
2021-04-04 18:41:41 -0500

    Agregue jquery
    Se agrego Jquery para agregar y borrar elementos al carrito de compras, contemplando los elementos del localstorage y DOM

--- js/productos.js
+++ js/productos.js
@@ -6,35 +6,45 @@ class Producto{
         this.imagen=imagen;
         this.descripcion=descripcion;
         this.puntuacion=puntuacion;
-    }
-    mostrarProducto(){
-        alert('el producto '+this.nombre+' tiene un valor de $'+this.precio);
-    }
-    
+    }    
 }
 function agregarCarrito(pos){
     let productosCarrito=[];
     if (localStorage.getItem('productos')){
-        console.log('esta creada');
         productosCarrito = JSON.parse(localStorage.getItem('productos'));
         console.log(productosCarrito);
         productosCarrito.push({
             id:productos[pos].id,
             nombre:productos[pos].nombre,
             precio:productos[pos].precio,
+            imagen:productos[pos].imagen,
         });    
     }else{
-        console.log("no esta creada");
             productosCarrito.push({
                 id:productos[pos].id,
                 nombre:productos[pos].nombre,
                 precio:productos[pos].precio,
+                imagen:productos[pos].imagen,
             });      
     }
     // console.log(productosCarrito);
     let yaagregado = document.getElementById(`add${pos}`);
     yaagregado.innerHTML="ya agregado";
     actualizarCarrito(productosCarrito);
+}
+function eliminarElemntoCarrito(id){
+    let productosCarrito=localStorage.getItem('productos');
+    productosCarrito = JSON.parse(productosCarrito);
+    let pos=0;
+    for(let i =0;i<productosCarrito.length;i++){
+        if(productosCarrito[i].id == id){
+            pos = i;
+        }
+    }
+    productosCarrito.splice(pos,1);
+    localStorage.removeItem('productos');
+    localStorage.setItem('productos',JSON.stringify(productosCarrito));
+    $(`#cart${id}`).remove();
 }
 
 function actualizarCarrito(productosCarrito){
@@ -44,6 +54,25 @@ function actualizarCarrito(productosCarrito){
     }else{
         localStorage.setItem('productos',JSON.stringify(productosCarrito));
     }
+    $("#tablacarrito").empty();
+    for(let i=0;i<productosCarrito.length;i++){
+        $("#tablacarrito").append(`
+        <div id="cart${productosCarrito[i].id}" class="layout-inline row">
+            <div class="col col-pro layout-inline">
+                <img class="imgcarrito" src='${productosCarrito[i].imagen}' alt="${productosCarrito[i].imagen}" />
+                <p style="margin-left: 10px;">${productosCarrito[i].nombre}</p>
+            </div>
+            <div class="col col-price col-numeric align-center ">
+                <p>$${productosCarrito[i].precio}</p>
+            </div>
+            <div class="col col-qty layout-inline">
+                <input type="numeric" value="1" />
+            </div>
+            <div class="col col-total col-numeric"><p> $$$$$$</p><button class="btn-danger" onclick="eliminarElemntoCarrito(${productosCarrito[i].id})">X</button></div>
+        </div>
+        `);
+    }
+    
 }
 
 let productos=[];
@@ -95,11 +124,39 @@ function pintarProductos(){
                 </div>
             </div>
         `
-    }
+    };
     contenedor.innerHTML=html;
 }
 
-crearProductos();
-pintarProductos();
+
+$(document).ready(function(){
+    crearProductos();
+    pintarProductos();
+
+    // carga del carrrito inicial 
+    if(localStorage.getItem('productos')){
+        let productosCarrito=JSON.parse(localStorage.getItem('productos'));
+        $("#tablacarrito").empty();
+        for(let i=0;i<productosCarrito.length;i++){
+            $("#tablacarrito").append(`
+            <div id="cart${productosCarrito[i].id}" class="layout-inline row">
+                <div class="col col-pro layout-inline">
+                    <img class="imgcarrito" src='${productosCarrito[i].imagen}' alt="${productosCarrito[i].imagen}" />
+                    <p style="margin-left: 10px;">${productosCarrito[i].nombre}</p>
+                </div>
+                <div class="col col-price col-numeric align-center ">
+                    <p>$${productosCarrito[i].precio}</p>
+                </div>
+                <div class="col col-qty layout-inline">
+                    <input type="numeric" value="1" />
+                </div>
+                <div class="col col-total col-numeric"><p> $$$$$$</p><button class="btn-danger" onclick="eliminarElemntoCarrito(${productosCarrito[i].id})">X</button></div>
+            </div>
+            `);
+    }
+    }
+    
+ });
+
 
 
